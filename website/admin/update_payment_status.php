440e9e751e486f1c2d165d644a38e1cc72a1113a
Nurul Najwa Adina <wawasakri0406@gmail.com>
2026-04-22 16:51:33 +0800

    Delete website directory

--- website/admin/update_payment_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/db_connect.php';
-
-$id = intval($_POST['id']);
-$status = $_POST['status'];
-
-$result = $conn->query("UPDATE payments SET status='$status' WHERE id=$id");
-
-echo json_encode(['success' => $result]);
